7cd6bf609211b09697b7eab516298357adc02a23
YohannTisserand <tisserand.yohann@gmail.com>
2021-04-16 08:49:43 +0100

    add some format

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -1,13 +1,13 @@
 require 'player'
 
 describe Player do
-  subject(:player) { described_class.new(name) }
-  let(:name) { 'Yohann' }
+  subject(:yohann) { described_class.new('Yohann') }
+  subject(:abdur) { described_class.new('Abdur') }
   
   
   context '#name' do
     it 'allows to return a name' do
-      expect(player.name).to eq name
+      expect(yohann.name).to eq 'Yohann'
     end
   end
 end
