bafb06a967d33d3c0bf199f6db97ff1bb07880a5
YohannTisserand <tisserand.yohann@gmail.com>
2021-04-16 09:41:27 +0100

    refactor names

--- spec/features/web_helpers.rb
+++ spec/features/web_helpers.rb
@@ -1,7 +1,7 @@
 def sign_in_and_play
   visit('/')
-  fill_in :player_1_name, with: 'Yohann'
-  fill_in :player_2_name, with: 'Abdur'
+  fill_in :player_1, with: 'Yohann'
+  fill_in :player_2, with: 'Abdur'
   click_button 'Submit'
   expect(page).to have_content 'Yohann vs Abdur'
 end
